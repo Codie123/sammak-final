b7dfc7704f65ed8b0d5aecf541746b45486b857e
fahil-haneef777 <fahilhaneef006@gmail.com>
2024-02-22 19:14:01 +0530

    video updated

--- sammak/component/SpecialSection/SpecialSection.jsx
+++ sammak/component/SpecialSection/SpecialSection.jsx
@@ -33,7 +33,7 @@ const SpecialSection = () => {
           <img src={whybody} alt="why our product" />
         </div>
         <div className={`${style.whyvideo}`}>
-          <video ref={videoRef} loop>
+          <video ref={videoRef} loop autoPlay muted>
             <source src={whyvideo} type="video/mp4" />
             Your browser does not support the video tag.
           </video>
