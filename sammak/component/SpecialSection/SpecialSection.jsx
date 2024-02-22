275bf21e6c8be7664fa5ef6f2cef3bc4c6e5dfd5
fahil-haneef777 <fahilhaneef006@gmail.com>
2024-02-22 12:57:51 +0530

    video

--- sammak/component/SpecialSection/SpecialSection.jsx
+++ sammak/component/SpecialSection/SpecialSection.jsx
@@ -1,14 +1,43 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import style from "./SpecialSection.module.css";
-
+import whyhead from "../../public/whyhead.png";
+import whybody from "../../public/whybody.png";
+import whyvideo from "../../public/main.mp4";
 const SpecialSection = () => {
-  const [data, setdata] = useState("");
+  const videoRef = useRef();
+  useEffect(() => {
+    const video = videoRef.current;
+
+    video.removeAttribute("controls");
+
+    video.play();
+
+    return () => {};
+  }, []);
   return (
     <>
-      <h2>Why</h2>
-      <div>
-        <div></div>
-        <div></div>
+      <h1
+        style={{
+          fontFamily: "Poppins",
+          fontWeight: "500",
+          textAlign: "center",
+          fontSize: "5vh",
+          marginBottom: "7vh",
+        }}
+      >
+        Why Our Products ?
+      </h1>
+      <div className={style.main}>
+        <div className={style.whybody}>
+          <img src={whyhead} alt="why our product" />
+          <img src={whybody} alt="why our product" />
+        </div>
+        <div className={`${style.whyvideo}`}>
+          <video ref={videoRef}>
+            <source src={whyvideo} type="video/mp4" />
+            Your browser does not support the video tag.
+          </video>
+        </div>
       </div>
     </>
   );
